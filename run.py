2df1016c9152bb74005df0907f3980aebf7525a3
nolepeople <natsuya.ren@gmail.com>
2021-09-15 15:45:41 +0700

    -

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from ourspace.setup import runserver
-
+import sys
 
 if __name__=="__main__":
-   runserver('ODg3NTY2Nzc0MDkzMTE1Mzk0.YUGA-Q.m1dqcrjWywUyFY1hB_pKnu9QoVk')
+   runserver(sys.argv[1])
