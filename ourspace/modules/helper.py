cd4a9bb2c6c74b368e95ce43f76a4366fbd66196
nolepeople <natsuya.ren@gmail.com>
2021-09-18 23:58:59 +0700

    -

--- ourspace/modules/helper.py
+++ ourspace/modules/helper.py
@@ -1,26 +1,19 @@
 import re
 
 
-class Helper:
-
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def ListToString(lists):
-        new_list = []
-        n = 0
-        for name in lists:
-            new_list.append(f'{n}. {name}')
-            n += 1
+def ListToString(lists):
+    new_list = []
+    n = 0
+    for name in lists:
+        new_list.append(f'{n}. {name}')
+        n += 1
         output = """```
 {}
 ```""".format('\n'.join(new_list))
-        return output
+    return output
 
-    @staticmethod
-    def getLabel(Input,labelName):
-        label = (lambda s: re.findall(r'-{}=(.*)'.format(labelName),s))(Input)
-        if label:
-            return label[0]
-        return None
+def getLabel(Input,labelName):
+    label = (lambda s: re.findall(r'-{}=(.*)'.format(labelName),s))(Input)
+    if label:
+        return label[0]
+    return None
